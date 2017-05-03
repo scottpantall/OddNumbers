cbb288d49b1f04304f121f19082c96c74633bed3
Scott Pantall <aspantall@gmail.com>
2017-05-02 18:00:18 -0600

    Works on CodeEval

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,10 @@ import java.io.*;
 
 public class Main {
     public static void main (String[] args) throws IOException {
-        
+        for(int i = 1; i < 100; i++) {
+            if(i % 2 == 1) {
+                System.out.println(i);
+            }
+        }
     }
 }
